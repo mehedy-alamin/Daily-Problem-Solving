f124c79a961728cdf6da83594c45a4ae721b3ccb
Mehedy Hasan Al-Amin <mehedy.codeconfig@gmail.com>
2025-04-24 10:32:28 +0600

    wordChack

---
+++ Find_long_word.js
@@ -0,0 +1,23 @@
+function findLongestWord(mySentence) {
+    singleWord = "";
+    longestWord = "";
+    wordStorage = [];
+
+    for (let i = 0; i <= mySentence.length; i++) {
+        if (mySentence[i] === " " || i === mySentence.length) {
+            if (singleWord.length > longestWord.length) {
+                longestWord = singleWord;
+            }
+            wordStorage.push(singleWord, singleWord.length);
+            singleWord = " ";
+        } else {
+            singleWord += mySentence[i];
+        }
+    }
+    console.log(
+        `The longest word is:"${longestWord}" and it has "${longestWord.length}" characters`
+    );
+    console.log(`The words in the sentence are: ${wordStorage}`);
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
